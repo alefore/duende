0510d91f0b7b680012ceec7208095a917cdbb26f
Alejandro Forero Cuervo <alefore@gmail.com>
2025-06-19 23:26:50 +0200

    Import missing file. Duh.

---
+++ src/random_key.py
@@ -0,0 +1,6 @@
+import random
+import string
+
+def GenerateRandomKey(length=20) -> str:
+    """Generate a random key of specified length."""
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
