6066d893a0287ce158cb95ceb15f9d9f93703aba
Alejandro Forero Cuervo <alefore@gmail.com>
2025-06-20 11:19:07 +0200

    Start defining classes to describe the syntax of commands.

--- src/agent_command.py
+++ src/agent_command.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import List, NamedTuple, Optional
+from collections import namedtuple
+from enum import Enum, auto
 
 
 class CommandOutput(NamedTuple):
@@ -13,6 +15,43 @@ class CommandInput(NamedTuple):
   arguments: List[str]
   # multiline_content does not include newline characters.
   multiline_content: Optional[List[str]] = None
+
+
+class ArgumentContentType(Enum):
+  PATH = auto()
+  REGEX = auto()
+  IDENTIFIER = auto()
+  STRING = auto()
+  OTHER = auto()
+
+
+class Argument(NamedTuple):
+  """Defines an argument with a name, type, and description."""
+  name: str
+  arg_type: ArgumentContentType
+  description: str
+
+
+class ArgumentMultiline(NamedTuple):
+  """Defines a multiline argument with an indicator if it's required and a description."""
+  required: bool = True
+  description: str = ""
+
+
+class CommandSyntax(NamedTuple):
+  """Defines the syntax structure for an AgentCommand."""
+
+  # A list of required arguments for the command.
+  required: List[Argument] = []
+
+  # A list of optional positional arguments for the command. These come after the required arguments.
+  optional: List[Argument] = []
+
+  # An optional final argument that can be repeated multiple times (e.g., for commands accepting multiple files).
+  repeatable_final: Optional[Argument] = None
+
+  # An optional multiline argument for the command, if applicable.
+  multiline: Optional[ArgumentMultiline] = None
 
 
 class AgentCommand(ABC):
