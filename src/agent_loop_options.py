63a71803f3651a04466cd66621e069cd5efbff46
Alejandro Cuervo <alefore@gmail.com>
2025-06-27 03:48:50 +0200

    Import src/agent_loop_options.py (duh!).

---
+++ src/agent_loop_options.py
@@ -0,0 +1,26 @@
+from typing import List, Optional, Pattern, NamedTuple
+
+from command_registry import CommandRegistry
+from confirmation import ConfirmationState
+from conversation import Conversation, ConversationFactory, Message
+from conversational_ai import ConversationalAI
+from file_access_policy import FileAccessPolicy
+from validation import ValidationManager
+
+
+class AgentLoopOptions(NamedTuple):
+  task_prompt_content: List[str]
+  conversation_factory: ConversationFactory
+  conversation_path: str
+  model: str
+  conversation: Conversation
+  start_message: Message
+  commands_registry: CommandRegistry
+  confirmation_state: ConfirmationState
+  file_access_policy: FileAccessPolicy
+  conversational_ai: ConversationalAI
+  confirm_regex: Optional[Pattern[str]] = None
+  confirm_done: bool = False
+  skip_implicit_validation: bool = False
+  validation_manager: Optional[ValidationManager] = None
+  do_review: bool = False
