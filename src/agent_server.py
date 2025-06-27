3f7647db1be3173afbcfa830750ee0d8fb414e4c
Alejandro Cuervo <alefore@gmail.com>
2025-06-27 02:32:18 +0200

    Add missing type annotations.

--- src/agent_server.py
+++ src/agent_server.py
@@ -2,7 +2,7 @@ import argparse
 from flask import Flask, request
 from flask_socketio import SocketIO
 import logging
-from typing import Any
+from typing import Any, Dict
 
 from args_common import CreateCommonParser
 from conversation import ConversationId
@@ -25,11 +25,16 @@ def Interact() -> Any:
   return app.send_static_file('index.html')
 
 
-def SendUpdate(server_state, data) -> None:
+def SendUpdate(server_state: WebServerState, data: Dict[str, Any]) -> None:
   message_count = data.get('message_count', 0)
+  conversation_id = data.get('conversation_id')
+  if conversation_id is None:
+    logging.error("SendUpdate: conversation_id is missing")
+    return
+
   logging.info(f"Received: request_update, message_count: {message_count}")
   server_state.SendUpdate(
-      data.get('conversation_id'), message_count, confirmation_required=None)
+      conversation_id, message_count, confirmation_required=None)
 
 
 def run_server() -> None:
@@ -38,14 +43,17 @@ def run_server() -> None:
   server_state = WebServerState(args, socketio)
 
   @socketio.on('confirm')
-  def handle_confirmation(data) -> None:
+  def handle_confirmation(data: Dict[str, Any]) -> None:
     logging.info("Received: confirm.")
-    conversation_id = data.get('conversation_id')
-    server_state.ReceiveConfirmation(data.get('confirmation'))
+    confirmation = data.get('confirmation')
+    if confirmation is None:
+      logging.error("handle_confirmation: confirmation is missing")
+      return
+    server_state.ReceiveConfirmation(confirmation)
     SendUpdate(server_state, data)
 
   @socketio.on('request_update')
-  def start_update(data) -> None:
+  def start_update(data: Dict[str, Any]) -> None:
     SendUpdate(server_state, data)
 
   @socketio.on('list_conversations')
