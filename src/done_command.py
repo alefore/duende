3026b421da96e1d096aaa6129ca9e519b520b219
Alejandro Forero Cuervo <alefore@gmail.com>
2025-07-02 22:22:07 +0200

    Add logging.

--- src/done_command.py
+++ src/done_command.py
@@ -1,18 +1,21 @@
 from typing import List, Any, Dict
+import logging
+
 from agent_command import AgentCommand, CommandOutput, CommandSyntax, ArgumentContentType, Argument
 
 
 class DoneCommand(AgentCommand):
 
   def Name(self) -> str:
-    return "done"
+    return self.Syntax().name
 
   def Syntax(self) -> CommandSyntax:
     return CommandSyntax(
-        name=self.Name(),
+        name="done",
         description="Signals that the current task is completed.",
         arguments=[],
     )
 
   def run(self, inputs: Dict[str, Any]) -> CommandOutput:
+    logging.info("Done command runs!")
     return CommandOutput(self.Name(), [], [], "Task completed.", task_done=True)
