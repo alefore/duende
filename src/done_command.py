30c07b4ed14f25d760264fc596d4dbc6eb27f1c7
Alejandro Forero Cuervo <alefore@gmail.com>
2025-10-03 10:25:44 +0200

    DoneCommand: Accept an optional ValidationManager.

--- src/done_command.py
+++ src/done_command.py
@@ -1,10 +1,15 @@
-from typing import List, Any, Dict
+from typing import List, Any, Dict, Optional
 import logging
 
 from agent_command import AgentCommand, CommandOutput, CommandSyntax, ArgumentContentType, Argument
+from validation import ValidationManager, ValidationResult
 
 
 class DoneCommand(AgentCommand):
+
+  def __init__(self,
+               validation_manager: Optional[ValidationManager] = None) -> None:
+    self._validation_manager = validation_manager
 
   def Name(self) -> str:
     return self.Syntax().name
@@ -18,4 +23,25 @@ class DoneCommand(AgentCommand):
 
   async def run(self, inputs: Dict[str, Any]) -> CommandOutput:
     logging.info("Done command runs!")
-    return CommandOutput(self.Name(), "", "", "Task completed.", task_done=True)
+    if not self._validation_manager:
+      return CommandOutput(
+          self.Name(), "", "", "Task completed.", task_done=True)
+
+    validation_result: ValidationResult = await self._validation_manager.Validate(
+    )
+    if not validation_result.success:
+      return CommandOutput(
+          self.Name(),
+          output=validation_result.output,
+          errors=validation_result.error,
+          summary=f"Validation failed.",
+          task_done=False,
+      )
+    else:
+      return CommandOutput(
+          self.Name(),
+          output=validation_result.output,
+          errors="",
+          summary=f"Validation succeeded.",
+          task_done=True,
+      )
