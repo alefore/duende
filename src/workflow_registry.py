8ff54469729fefe4582490f5612f026afb270c7e
Alejandro Forero Cuervo <alefore@gmail.com>
2025-10-20 22:01:42 +0200

    Hook dm-tests workflow.

--- src/workflow_registry.py
+++ src/workflow_registry.py
@@ -1,10 +1,12 @@
 from implement_workflow import ImplementAndReviewWorkflowFactory
 from agent_workflow import AgentWorkflowFactoryContainer
 from code_specs_workflow import CodeSpecsWorkflowFactory
+from code_specs_tests_skeleton import CodeSpecsTestsSkeletonWorkflowFactory
 
 
 def StandardWorkflowFactoryContainer() -> AgentWorkflowFactoryContainer:
   output = AgentWorkflowFactoryContainer()
   output.add(ImplementAndReviewWorkflowFactory())
   output.add(CodeSpecsWorkflowFactory())
+  output.add(CodeSpecsTestsSkeletonWorkflowFactory())
   return output
