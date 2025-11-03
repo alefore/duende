3efd3f2b0dae9adede5142e6917af0bd7e9805b3
Alejandro Forero Cuervo <alefore@gmail.com>
2025-11-03 18:28:31 +0100

    Update imports to new locations.

--- src/code_specs_workflow.dm.py
+++ src/code_specs_workflow.dm.py
@@ -17,8 +17,9 @@ from agent_loop_options import AgentLoopOptions
 from agent_loop_options import BaseAgentLoopFactory
 from agent_workflow import AgentWorkflow, AgentWorkflowFactory
 from agent_workflow_options import AgentWorkflowOptions
-from code_specs import FileExtension, MarkerChar, MarkerImplementation, MarkerName, PathAndValidator, RepeatedExpandedMarkersError, Validator, comment_string, get_expanded_markers, get_markers, relevant_paths_variable
+from code_specs import FileExtension, MarkerChar, MarkerImplementation, MarkerName, RepeatedExpandedMarkersError, Validator, comment_string, get_expanded_markers, get_markers, relevant_paths_variable
 from code_specs_agent import prepare_command_registry, prepare_initial_message, run_agent_loop
+from code_specs_path_and_validator import PathAndValidator
 from conversation import Conversation, ConversationId, ConversationFactory
 from conversation_state import ConversationState
 from done_command import DoneCommand, DoneValuesValidator
