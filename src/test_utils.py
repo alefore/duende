3f5176c65f849a996a1338de4d05146c39c8a858
Alejandro Forero Cuervo <alefore@gmail.com>
2025-11-05 10:38:04 +0100

    Add slow_conversation_factory_options.

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,6 +1,8 @@
+import asyncio
+
 from file_access_policy import FileAccessPolicy
 from confirmation import ConfirmationManager, ConfirmationState
-from conversation import ConversationId
+from conversation import ConversationId, ConversationFactoryOptions
 from selection_manager import SelectionManager
 
 
@@ -26,3 +28,11 @@ class FakeConfirmationManager(ConfirmationManager):
 
 class FakeSelectionManager(SelectionManager):
   pass  # Default implementation is fine for now
+
+
+def slow_conversation_factory_options() -> ConversationFactoryOptions:
+
+  async def sleep(_: ConversationId) -> None:
+    await asyncio.sleep(0.1)
+
+  return ConversationFactoryOptions(on_message_added_callback=sleep)
