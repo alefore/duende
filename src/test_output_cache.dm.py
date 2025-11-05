008416874718e38f46d799b2310d9b0b2a3a7316
Alejandro Forero Cuervo <alefore@gmail.com>
2025-11-05 23:28:35 +0100

    Prepare template for tests for output_cache.

--- src/test_output_cache.dm.py
+++ src/test_output_cache.dm.py
@@ -1,12 +1,20 @@
-import unittest
+# Suggested validator: mypy $DMPATH && $PYTHON -m pytest $DMPATH
+
 import asyncio
+import os
+import pathlib
+import unittest
+
+import output_cache
+
 
 class OutputCacheTest(unittest.IsolatedAsyncioTestCase):
 
   def test_get_path_for_key_output_is_in_base_dir(self) -> None:
     pass  # {{🍄 Output is a file directly in `self._base_dir` (in `_get_path_for_key`).}}
 
-  def test_get_path_for_key_changing_key_results_in_different_hash(self) -> None:
+  def test_get_path_for_key_changing_key_results_in_different_hash(
+      self) -> None:
     pass  # {{🍄 Changing any of the parameters in `key` results in a different hash (in `_get_path_for_key`).}}
 
   async def test_save_uses_async_io(self) -> None:
@@ -14,6 +22,7 @@ class OutputCacheTest(unittest.IsolatedAsyncioTestCase):
 
   async def test_load_uses_async_io(self) -> None:
     pass  # {{🍄 Uses async io (in `load`).}}
+
 
 if __name__ == '__main__':
   unittest.main()
