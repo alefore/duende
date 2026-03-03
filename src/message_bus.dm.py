d3f3c611b1588e4d54baf7287444a0e4e445c9fb
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-04 00:19:01 +0100

    Turn MessageBus into a class.

--- src/message_bus.dm.py
+++ src/message_bus.dm.py
@@ -1,15 +1,15 @@
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import dataclasses
 import logging
 import pathlib
 import sqlite3
-from typing import NewType
+from typing import Callable, NewType, TypeVar
 
 from swarm_types import AgentName
 
 SenderName = AgentName | None
 RecipientName = AgentName | None
-MessageBus = NewType('MessageBus', sqlite3.Connection)
 MessageId = NewType('MessageId', int)  # Unique ID across the bus.
 SessionId = NewType('SessionId', str)  # A UUID.
 
@@ -24,39 +24,61 @@ class Message:
   body: str
 
 
-async def open_bus(path: pathlib.Path) -> MessageBus:
-  """Connects to the bus, potentially initializing it.
-
-  If the database is empty (or does not exist), creates the expected tables
-  (see message_bus.sql for details).
-
-  Passes timeout=20.0 and isolation_level=None when connecting.
-  """
-  raise NotImplementedError()  # {{🍄 open bus}}
+T = TypeVar("T")
 
 
-async def wait_for_new_messages(
-    db: MessageBus, recipients: list[RecipientName]) -> list[Message]:
-  """Polls db until new messages arrive for the recipients listed.
+class MessageBus:
 
-  A new message is one with status set to 'new'.
+  def __init__(self, path: pathlib.Path) -> None:
+    self._path = path
+    self._executor = ThreadPoolExecutor(max_workers=1)
+    # _connection is only accessed by threads running in _executor.
+    self._connection: sqlite3.Connection | None = None
 
-  Initially polls after one second, growing polling frequency exponentially up
-  to one minute.
+  async def _run_in_thread(self, func: Callable[..., T], *args) -> T:
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(self._executor, func, *args)
 
-  Calls logging.info as it polls/sleeps.
-  """
-  raise NotImplementedError()  # {{🍄 wait for new messages}}
+  async def open(self) -> None:
+    """Connects to the bus, potentially initializing it.
 
+    If the database is empty (or does not exist), creates the expected tables
+    (see message_bus.sql for details).
 
-async def mark_message_as_seen(db: MessageBus, message_id: MessageId) -> None:
-  """Updates the `status` field in self._messages_db to `seen`."""
-  raise NotImplementedError()  # {{🍄 mark message as seen}}
+    Passes timeout=20.0 and isolation_level=None when connecting.
+    """
 
+    def _open() -> sqlite3.Connection:
+      raise NotImplementedError()  # {{🍄 init db}}
 
-async def write_new_message(db: MessageBus, message: Message) -> MessageId:
-  """Adds a new message to the database.
+    await self._run_in_thread(_open)
 
-  The MessageId will be overwritten (based on database state) and returned.
-  """
-  raise NotImplementedError()  # {{🍄 write new message}}
+  async def wait_for_new_messages(
+      self, recipients: list[RecipientName]) -> list[Message]:
+    """Polls db until new messages arrive for the recipients listed.
+
+    A new message is one with status set to 'new'.
+
+    Initially polls after one second, growing polling frequency exponentially up
+    to one minute.
+
+    Calls logging.info as it polls/sleeps.
+    """
+
+    def _load_messages() -> list[Message]:
+      """May return empty list."""
+      raise NotImplementedError()  # {{🍄 load messages}}
+
+    # Call _load_messages (with exponential back-off) in _executor until it returns messages:
+    raise NotImplementedError()  # {{🍄 poll for new messages}}
+
+  async def mark_message_as_seen(self, message_id: MessageId) -> None:
+    """Updates the `status` field in self._messages_db to `seen`."""
+    raise NotImplementedError()  # {{🍄 mark message as seen}}
+
+  async def write_new_message(self, message: Message) -> MessageId:
+    """Adds a new message to the database.
+
+    The MessageId will be overwritten (based on database state) and returned.
+    """
+    raise NotImplementedError()  # {{🍄 write new message}}
