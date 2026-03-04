2bcbce064d1f597b1509dd2ac2c32989ef18a849
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-04 10:28:23 +0100

    Actually import spec for swarm config module.

---
+++ src/swarm_config.dm.py
@@ -0,0 +1,30 @@
+import aiofiles
+import dataclasses
+import json
+import pathlib
+
+from swarm_types import AgentName
+
+
+@dataclasses.dataclass(frozen=True)
+class AgentIdentityConfig:
+  # Unique name of this agent
+  name: AgentName
+
+  capability: list[str]
+
+  prompt_path: pathlib.Path
+
+  file_access_policy_regex: str
+
+
+@dataclasses.dataclass(frozen=True)
+class SwarmConfig:
+  agents: dict[AgentName, AgentIdentityConfig]
+  # Path to the SQLite DB containing the messages queue.
+  message_bus_path: pathlib.Path
+
+
+async def load_config(path: pathlib.Path) -> SwarmConfig:
+  """Loads the configuration from JSON file in `path`."""
+  raise NotImplementedError()  # {{🍄 load config}}
