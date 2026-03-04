f7d3945d55f2d63db9978460f1ce880765fbde06
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-04 22:23:39 +0100

    Spec: telegram_consumer, telegram_authorized_users.

--- src/swarm_config.dm.py
+++ src/swarm_config.dm.py
@@ -2,6 +2,9 @@ import aiofiles
 import dataclasses
 import json
 import pathlib
+from typing import NewType
+
+TelegramId = NewType("TelegramId", int)
 
 from swarm_types import AgentName
 
@@ -26,7 +29,20 @@ class SwarmConfig:
 
   telegram_token: str | None
 
+  telegram_consumer: AgentName | None
+
+  telegram_authorized_users: list[TelegramId]
+
 
 async def load_config(path: pathlib.Path) -> SwarmConfig:
-  """Loads the configuration from JSON file in `path`."""
+  """Loads the configuration from JSON file in `path`.
+
+  If `telegram_token` is set:
+
+  * `telegram_authorized_users` must not be empty.
+  * `telegram_consumer` must be set to a key in `agents`.
+
+  If `telegram_authorized_users` or `telegram_consumer` is set, `telegram_token`
+  must also be set.
+  """
   raise NotImplementedError()  # {{🍄 load config}}
