80c609c8ef1715e20cfe9cc251231ae692bf7bb8
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-04 22:22:52 +0100

    Spec: PublishMessageCommand.

--- src/swarm_commands.dm.py
+++ src/swarm_commands.dm.py
@@ -1,13 +1,14 @@
 from agent_command import AgentCommand, CommandInput, CommandOutput, CommandSyntax, Argument, ArgumentContentType, REASON_VARIABLE, VariableMap, VariableName, VariableValueInt
 from file_access_policy import FileAccessPolicy
-from message_bus import Message as BusMessage, MessageBus, MessageId, SenderName, SessionId, write_new_message
+from message_bus import Message as BusMessage, MessageBus, MessageId, SenderName, SessionId
+from swarm_types import AgentName
 
 
 class DisplayInfoCommand(AgentCommand):
 
   def __init__(self, message_bus: MessageBus, sender: SenderName,
                session_id: SessionId) -> None:
-    raise NotImplementedError()  # {{🍄 store private fields}}
+    raise NotImplementedError()  # {{🍄 display info store private fields}}
 
   def Name(self) -> str:
     return self.Syntax().name
@@ -31,4 +32,41 @@ class DisplayInfoCommand(AgentCommand):
 
     {{🦔 The message's recipient is None.}}
     """
-    raise NotImplementedError()  # {{🍄 provide confirmation}}
+    raise NotImplementedError()  # {{🍄 display info rum}}
+
+
+class PublishMessageCommand(AgentCommand):
+
+  def __init__(self, message_bus: MessageBus, sender: SenderName,
+               session_id: SessionId) -> None:
+    raise NotImplementedError()  # {{🍄 publish message store private fields}}
+
+  def Name(self) -> str:
+    return self.Syntax().name
+
+  @classmethod
+  def Syntax(self) -> CommandSyntax:
+    return CommandSyntax(
+        name="publish_message",
+        description="Publishes a new message into the message bus. "
+        "The message will be consumed by an agent.",
+        arguments=[
+            REASON_VARIABLE,
+            Argument(
+                name=VariableName("message"),
+                arg_type=ArgumentContentType.STRING,
+                description="The body of the message to publish.",
+                required=True),
+            Argument(
+                name=VariableName("recipient"),
+                arg_type=ArgumentContentType.STRING,
+                description="The name of the agent that should consume the message."
+            )
+        ])
+
+  async def run(self, inputs: VariableMap) -> CommandOutput:
+    """Call write_new_message with a new message.
+
+    {{🦔 The message's recipient is always set.}}
+    """
+    raise NotImplementedError()  # {{🍄 publish message run}}
