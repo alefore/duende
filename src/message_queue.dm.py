c31f8df5e4ae31c439ac1ea595340b60115e2125
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-05 23:32:39 +0100

    Import new AgentMessageQueue class.

---
+++ src/message_queue.dm.py
@@ -0,0 +1,25 @@
+import asyncio
+
+
+class AgentMessageQueue:
+
+  def __init__(self):
+    self._lock = asyncio.Lock()  # Guards access to _data.
+    # The class can be in any of two states:
+    #
+    # * Somebody is waiting for messages: In this case, self._data is set to a
+    #   future to be notified when the first message arrives.
+    #
+    # * Nobody is waiting for messages: In this case, self._data is set to a
+    #   list, where messages will be collected.
+    #
+    # Calls to `read` may not happen concurrently.
+    self._data: list[str] | asyncio.Future[list[str]] = []
+
+  async def push(self, message: str) -> None:
+    """Appends a message to the queue."""
+    raise NotImplementedError()  # {{🍄 push}}
+
+  async def read(self) -> list[str]:
+    """Returns a non-empty list of messages (possibly blocking)."""
+    raise NotImplementedError()  # {{🍄 read}}
