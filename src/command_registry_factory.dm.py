8385844c31b7cef686fa27d764c776aca5fcf208
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-07 08:14:06 +0100

    Start creating CommandRegistryConfig.

---
+++ src/command_registry_factory.dm.py
@@ -0,0 +1,120 @@
+import asyncio
+import aiofiles
+import dataclasses
+import json
+import pathlib
+from typing import Callable
+import sys
+
+from agent_command import AgentCommand, CommandOutput
+from agent_command_helpers import FormatHelp
+from command_registry import CommandRegistry
+from file_access_policy import FileAccessPolicy
+from validation import ValidationManager
+from validate_command import ValidateCommand
+from read_file_command import ReadFileCommand
+from list_files_command import ListFilesCommand
+from write_file_command import WriteFileCommand
+from search_file_command import SearchFileCommand
+from select_commands import SelectCommand, SelectOverwriteCommand
+from selection_manager import SelectionManager
+from select_python import SelectPythonCommand
+from replace_python_command import ReplacePythonCommand
+from git_commands import ResetFileCommand, CheckGitRepositoryState, GitRepositoryState
+from task_command import TaskInformation
+from done_command import DoneCommand
+from answer_command import AnswerCommand
+from shell_command_command import ShellCommandCommand
+import sys
+
+
+def _create_base_registry(
+    file_access_policy: FileAccessPolicy) -> CommandRegistry:
+  registry = CommandRegistry()
+  registry.Register(ReadFileCommand(file_access_policy))
+  registry.Register(ListFilesCommand(file_access_policy))
+  registry.Register(SearchFileCommand(file_access_policy))
+  return registry
+
+
+def create_ask_command_registry(
+    file_access_policy: FileAccessPolicy) -> CommandRegistry:
+  registry = CommandRegistry()
+  registry.Register(ReadFileCommand(file_access_policy))
+  registry.Register(ListFilesCommand(file_access_policy))
+  registry.Register(SearchFileCommand(file_access_policy))
+  registry.Register(AnswerCommand())
+  return registry
+
+
+@dataclasses.dataclass(frozen=True)
+class CommandRegistryConfig:
+  allow_shell: bool
+
+
+async def load_config(path: pathlib.Path) -> CommandRegistryConfig:
+  """Loads the configuration from JSON file in `path`."""
+  raise NotImplementedError()  # {{🍄 load config}}
+
+
+async def create_command_registry(
+    file_access_policy: FileAccessPolicy,
+    config: CommandRegistryConfig,
+    validation_manager: ValidationManager | None,
+    start_new_task: Callable[[TaskInformation], CommandOutput],
+    git_dirty_accept: bool = False,
+    can_write: bool = True,
+    can_start_tasks: bool = True) -> CommandRegistry:
+  registry = _create_base_registry(file_access_policy)
+
+  registry.Register(DoneCommand(arguments=[]))
+
+  git_state = await CheckGitRepositoryState()
+  if git_state == GitRepositoryState.CLEAN:
+    registry.Register(ResetFileCommand(file_access_policy, validation_manager))
+  elif git_state == GitRepositoryState.NOT_CLEAN:
+    if git_dirty_accept:
+      print(
+          "Info: The repository has uncommitted changes; ignoring due to command-line flag --git-dirty-accept.",
+          file=sys.stderr)
+    else:
+      print("Error: The repository has uncommitted changes.", file=sys.stderr)
+      sys.exit(1)
+
+  if validation_manager:
+    registry.Register(ValidateCommand(validation_manager))
+
+  if config.allow_shell:
+    registry.Register(ShellCommandCommand())
+
+  enable_select = False
+
+  selection_manager = SelectionManager()
+  if can_write:
+    registry.Register(
+        WriteFileCommand(validation_manager, selection_manager, None))
+
+  if enable_select:
+    for use_regex in [True, False]:
+      registry.Register(
+          SelectCommand(file_access_policy, selection_manager, use_regex))
+
+    if can_write:
+      registry.Register(
+          SelectOverwriteCommand(selection_manager, validation_manager))
+
+    registry.Register(
+        SelectPythonCommand(file_access_policy, selection_manager))
+    if can_write:
+      registry.Register(
+          ReplacePythonCommand(file_access_policy, validation_manager))
+
+  #if can_start_tasks:
+  #  registry.Register(TaskCommand(start_new_task))
+
+  return registry
+
+
+def CreateReviewCommandRegistry(
+    file_access_policy: FileAccessPolicy) -> CommandRegistry:
+  return _create_base_registry(file_access_policy)
