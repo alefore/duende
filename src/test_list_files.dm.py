e489ade641cdbbe83317ed02ab75893f8a07465d
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-10 13:47:46 +0100

    Enable more static typing.

--- src/test_list_files.dm.py
+++ src/test_list_files.dm.py
@@ -15,13 +15,13 @@ from list_files import list_all_files
 class ListAllFilesTest(unittest.IsolatedAsyncioTestCase):
   """Contains a good set of tests for `list_all_files`."""
 
-  def setUp(self):
+  def setUp(self) -> None:
     self.original_cwd = os.getcwd()
     self.temp_dir = pathlib.Path(tempfile.mkdtemp())
     os.chdir(self.temp_dir)
     self._setUpInitialStructure()
 
-  def _setUpInitialStructure(self):
+  def _setUpInitialStructure(self) -> None:
     """Sets up the following structure (with empty files):
 
     animals/mammals: dog.txt, cat.txt, fox.txt
@@ -30,46 +30,46 @@ class ListAllFilesTest(unittest.IsolatedAsyncioTestCase):
     """
     # {{🍄 create directory structure}}
 
-  def tearDown(self):
+  def tearDown(self) -> None:
     os.chdir(self.original_cwd)
     shutil.rmtree(self.temp_dir)
 
-  async def testRoot(self):
+  async def testRoot(self) -> None:
     """Root search (".") finds expected number of files."""
     # {{🍄 root}}
 
-  async def testRootFormat(self):
+  async def testRootFormat(self) -> None:
     """The output of root search (".") is as expected.
 
     Expected format: "fruits/banana.md"."""
     # {{🍄 expected output root}}
 
-  async def testEmpty(self):
+  async def testEmpty(self) -> None:
     """No files are returned for a search in an empty directory."""
     # {{🍄 empty}}
 
-  async def testFlat(self):
+  async def testFlat(self) -> None:
     """Search in a flat directory returns expected number of outputs."""
     # {{🍄 flat}}
 
-  async def testFlatFormat(self):
+  async def testFlatFormat(self) -> None:
     """Search in flat directory returns entries with expected format.
 
     Expected format: "animals/birds/condor.txt"."""
     # {{🍄 flat format}}
 
-  async def testNestedDirs(self):
+  async def testNestedDirs(self) -> None:
     """Search in animals outputs five entries in the expected format."""
     # {{🍄 nested dirs}}
 
-  async def testFileAccessPolicy(self):
+  async def testFileAccessPolicy(self) -> None:
     """Search in root directory only outputs entries allowed by access policy.
 
     Creates a regex file access policy matching two files and validates that
     the outputs are exactly as expected."""
     # {{🍄 file access policy}}
 
-  async def testFileAccessPolicyNoMatch(self):
+  async def testFileAccessPolicyNoMatch(self) -> None:
     """Runs successfully if file access policy doesn't match anything."""
     # {{🍄 file access policy no match}}
 
